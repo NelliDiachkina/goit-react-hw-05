a38cfc55c8508b37c1e405850eed7c7e00a756cb
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-07-11 17:15:41 +0300

    update

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,11 +4,10 @@ import css from './Loader.module.css';
 export default function Loader() {
   return (
     <div className={css.wrapper}>
-      <p>Loading ...</p>
       <InfinitySpin
         visible={true}
         width="200"
-        color="#ffffffde"
+        color="#eb8108"
         ariaLabel="infinity-spin-loading"
       />
     </div>
